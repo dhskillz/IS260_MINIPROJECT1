035f93ad7c700bc61c3417789ad4e6441f8cb251
dhskillz <dougskil@comfsm.fm>
2015-09-23 16:39:49 +1100

    Create Miniproject1-skilling.cpp

---
+++ Miniproject1-skilling.cpp
@@ -0,0 +1,13 @@
+// Submitted By: Douglas skilling
+// submitted to : Edper Castro
+// Mini Project 1
+#include <iostream>
+#include <string>
+using namespace std
+int main()
+{
+  std::string name;
+  std::cout << "What is your name? ";
+  getline (std::cin, name);
+  std::cout << "Hello, " << name << "!\n";
+}
